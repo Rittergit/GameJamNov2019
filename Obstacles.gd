f607c84210c2fd6e244f5b39219211f7ba174e59
jeckgit <joerg_eckhold@web.de>
2019-11-09 19:12:12 +0100

    should create obtacle properly, not visible for some reason.

--- Obstacles.gd
+++ Obstacles.gd
@@ -1,7 +1,10 @@
 extends Node
 
+var Spawn = preload("Spawn.tscn")
+
 var obstacles_node_children = []
 var global_timer = 0
+var moving_obstacles_arr = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,20 +16,27 @@ func _process(delta):
 	global_timer = (global_timer+1)%60 
 	if global_timer == 0:
 		var moving_obstacles = get_moving_obstacles(obstacles_node_children)
-		for obstacle in moving_obstacles:
-			obstacle.position.y += 10
+		moving_obstacles_arr.append(moving_obstacles)
+		for obstacles in moving_obstacles_arr:
+			for obstacle in obstacles:
+				if obstacle == $_NULL:
+					continue
+				obstacle.position.y += 10
+				print(obstacle, obstacle.position.y)
 	
 func get_moving_obstacles(obstacles):
 	var deltion_num = randi()%10
 	var deltion_nums = [deltion_num, deltion_num + 1]
-	var to_sent_obstacles = []
+	var to_sent_obstacles = []	
 	
 	if obstacles.size() <= 0:
 		return
 	for idx in range(obstacles.size()):
 		if not deltion_nums.has(idx):
-			to_sent_obstacles.append(obstacles[idx])
+			var obst = Spawn.instance()
+			obst.position = obstacles[idx].position
+			to_sent_obstacles.append(Spawn.instance())
 		else:
 			to_sent_obstacles.append($_NULL)
-	return obstacles
+	return to_sent_obstacles
 
