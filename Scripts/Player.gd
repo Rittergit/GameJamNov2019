b98b59345077628afc9aab4a2c454ba245adf5ad
Tim Ritter <tim.ritter@signon-group.com>
2019-11-10 13:06:04 +0100

    menu

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,17 +1,21 @@
 extends KinematicBody2D
 
 export (int) var speed = 100
+var rightBorder = 48
+var leftBorder = -32
 
 var velocity = Vector2()
 
 func get_input():
     velocity = Vector2()
-    if Input.is_action_pressed('right'):
-        velocity.x += 1
-    if Input.is_action_pressed('left'):
-        velocity.x -= 1
+    if Input.is_action_just_pressed('right'):
+        if position.x <= rightBorder:
+            position.x += 16
+    if Input.is_action_just_pressed('left'):
+        if position.x >= leftBorder:
+            position.x -= 16
     velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
     get_input()
-    velocity = move_and_slide(velocity)
+    #velocity = move_and_slide(velocity)
